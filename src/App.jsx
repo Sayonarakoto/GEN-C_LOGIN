9af0f0b36f097ebda07074a635c497c947e63d9a
Najeeb <playitforpubg@gmail.com>
2025-09-03 14:34:08 +0530

    App change

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signup from './Pages/Signup';
 import Forgetpass from './Pages/Forgetpass';
 import Frontpage from './Pages/Frontpage';
-import StudentDashboard from './Dashbaords/StudentDashboard'
+import StudentDashboard from './Dashbaords/StudentDashboard';
 import FacultyDashboard from './Dashbaords/FacultyDashboard';
 import StudentEntryForm from './faculty/StudentEntryForm';
 import EmailInput from './Pages/EmailInput';
@@ -13,14 +13,14 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/sent-reset" element={<EmailInput/>} />
-        <Route path="/register" element={<Register/>} />
-        <Route path="/home" element={<Frontpage />} />
+        <Route path="/" element={<Frontpage />} />   {/* 👈 changed from /home */}
+        <Route path="/sent-reset" element={<EmailInput />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/signin" element={<Signup />} />
         <Route path="/forget" element={<Forgetpass />} />
-          <Route path='/faculty' element={<FacultyDashboard/>}/>
-        <Route path='/student' element={<StudentDashboard/>}/>
-        <Route path='/StudentForm' element={<StudentEntryForm/>} />
+        <Route path="/faculty" element={<FacultyDashboard />} />
+        <Route path="/student" element={<StudentDashboard />} />
+        <Route path="/StudentForm" element={<StudentEntryForm />} />
       </Routes>
     </Router>
   );
